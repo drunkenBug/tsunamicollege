045544ad080e86656ef5d353f8c640897469083b
root <dominikormann@outlook.com>
2021-04-11 20:53:04 +0000

    ip updated

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,10 +5,10 @@ module.exports = {
       proxy: {
         '^/api':{
           // //for production:
-          //target: 'http://142.93.169.96:3080',
+          target: 'http://134.209.242.22:3080',
 
           // for local testing:
-          target:'http://localhost:3080',
+          //target:'http://localhost:3080',
           
           changeOrigin: true
         },
